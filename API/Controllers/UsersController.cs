349d3b5bbbc06c6d67bc36e9a051b20a8ca50061
Alejandro Robledo <alexrobledo209@gmail.com>
2024-11-21 11:28:28 -0600

    Parcial03 - Update user repository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 namespace API.Controllers;
+
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using AutoMapper;
@@ -9,9 +11,11 @@ using Microsoft.AspNetCore.Mvc;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
-    public UsersController(IUserRepository repository)
+    private readonly IMapper _mapper;
+    public UsersController(IUserRepository repository, IMapper mapper)
     {
         _repository = repository;
+        _mapper = mapper;
     }
 
     [HttpGet]
@@ -32,4 +36,25 @@ public class UsersController : BaseApiController
         return member;
     }
 
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateRequest request)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null)
+        {
+            return BadRequest("No username found in token");
+        }
+        var user = await _repository.GetByUsernameAsync(username);
+        if (user == null)
+        {
+            return BadRequest("Could not find user");
+        }
+        _mapper.Map(request, user);
+        _repository.Update(user);
+        if (await _repository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+        return BadRequest("Update user failed!");
+    }
 }
