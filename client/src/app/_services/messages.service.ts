a41db328394ed2a1c4b0b0cbad52d71f0a6ae886
Alejandro Robledo <alexrobledo209@gmail.com>
2025-05-08 11:44:54 -0600

    Parcial05 - Add MessagesService to SPA

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -15,23 +15,11 @@ export class MessagesService {
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = setPaginationHeaders(pageNumber, pageSize);
-    params = params.append("Container", container.toLocaleLowerCase()); 
+    params = params.append("Container", container);
 
     return this.http.get<Message[]>(this.baseUrl + "messages",
       { observe: "response", params }).subscribe({
         next: response => setPaginationResponse(response, this.paginatedResult)
       });
-  }
-
-  getMessageThread(username: string) { 
-    return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username); 
-  }
-
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + "messages", { recipientUsername: username, content });
-  } 
-
-  deleteMessage(id: number) {
-    return this.http.delete(this.baseUrl + "messages/" + id);
   }
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,55 +1,25 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../_services/messages.service';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { FormsModule } from '@angular/forms';
-import { TimeagoModule } from 'ngx-timeago';
-import { Message } from '../_models/message';
-import { RouterModule } from '@angular/router';
-import { PaginationModule } from 'ngx-bootstrap/pagination'; 
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterModule, PaginationModule], 
+  imports: [],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 export class MessagesComponent implements OnInit {
   messagesService = inject(MessagesService);
-  container = "Inbox"; 
+  container = "outbox";
   pageNumber = 1;
-  pageSize = 5; 
-  isOutbox = this.container === "Outbox"; 
+  pageSize = 10;
 
   ngOnInit(): void {
     this.loadMessages();
   }
 
   loadMessages() {
-    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container.toLocaleLowerCase()); 
-  }
-
-  deleteMessage(id: number) {
-    this.messagesService.deleteMessage(id).subscribe({
-      next: _ => {
-        this.messagesService.paginatedResult.update(prev => {
-          if (prev && prev.items) {
-            prev.items.splice(prev.items.findIndex(m => m.id == id), 1);
-            return prev;
-          }
-          return prev;
-        });
-      }
-    });
-  }
-
-  getRoute(message: Message) {
-    if (this.isOutbox) { 
-      return `/members/${message.recipientUsername}`; 
-    }
-    else {
-      return `/members/${message.senderUsername}`;
-    }
+    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container);
   }
 
   pageChanged(event: any) {
