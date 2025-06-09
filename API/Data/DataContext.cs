d9af52351cd13714575367480457c75ef761a935
Alejandro Robledo <alexrobledo209@gmail.com>
2025-06-09 00:06:29 -0600

    Parcial06 - Add Message Groups tracking

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,6 +7,8 @@ public class DataContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
     public DbSet<Message> Messages { get; set; }
+    public DbSet<MessageGroup> Groups { get; set; }
+    public DbSet<Connection> Connections { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
