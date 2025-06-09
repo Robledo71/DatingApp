139f8f07dcc8435d243009141b002727ec39e17b
Alejandro Robledo <alexrobledo209@gmail.com>
2025-06-09 13:28:02 -0600

    Parcial06 - Add Unit of Work pattern

---
+++ API/Data/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+namespace API.UnitOfWork;
+using API.Data;
+
+public interface IUnitOfWork
+{
+    public IMessageRepository MessageRepository { get; }
+    public ILikesRepository LikesRepository { get; }
+    public IUserRepository UserRepository { get; }
+    public Task<bool> Complete();
+    public bool HasChanges();
+}
