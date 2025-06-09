df09f3e1817fb590abe34cb297a16d3feb8898d3
Alejandro Robledo <alexrobledo209@gmail.com>
2025-06-09 00:10:16 -0600

    Parcial06 - Update message hub with group tracking

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,18 @@
 namespace API;
+
 using API.Data;
+using API.DataEntities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
+[ExcludeFromCodeCoverage]
 public class Program
 {
     public static async Task Main(string[] args)
@@ -15,14 +20,16 @@ public class Program
         var host = CreateHostBuilder(args).Build();
         using var scope = host.Services.CreateScope();
         var services = scope.ServiceProvider;
+
         try
         {
             var context = services.GetRequiredService<DataContext>();
-            // var userManager = services.GetRequiredService<UserManager<AppUser>>();
-            // var roleManger = services.GetRequiredService<RoleManager<AppRole>>();
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            var roleManger = services.GetRequiredService<RoleManager<AppRole>>();
 
             await context.Database.MigrateAsync();
-            await Seed.SeedUsersAsync(context); // (userManager, roleManger);
+            await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+            await Seed.SeedUsersAsync(userManager, roleManger);
         }
         catch (Exception ex)
         {
@@ -30,8 +37,9 @@ public class Program
             logger.LogError(ex, "An error has ocurred during migration/seeding");
         }
 
-    await host.RunAsync();
+        await host.RunAsync();
     }
+
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
